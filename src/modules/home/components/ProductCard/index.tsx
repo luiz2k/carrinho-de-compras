913ffd512d5322bb888f80adc1f32a6f36e781a6
luiz2k <luizteles934@gmail.com>
2024-08-14 20:25:07 -0300

    Criar um card para exibir os produtos

---
+++ src/modules/home/components/ProductCard/index.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
+import { useProductStore } from "@/modules/shared/stores/productStore";
+import Image from "next/image";
+
+import type { CardProps } from "./types";
+
+export function ProductCard({
+  id,
+  title,
+  description,
+  image,
+  price,
+}: CardProps) {
+  const { add } = useProductStore();
+
+  return (
+    <>
+      {image && (
+        <article title={title}>
+          <Card className="w-[275px] pt-6">
+            <CardContent>
+              <div className="overflow-hidden rounded-lg">
+                <Image
+                  src={image}
+                  alt={title}
+                  width={225}
+                  height={225}
+                  className="mx-auto duration-200 hover:scale-105"
+                />
+              </div>
+
+              <CardTitle className="line-clamp-1 pt-2.5 text-base">
+                {title}
+              </CardTitle>
+            </CardContent>
+
+            <CardFooter>
+              <p>R$ {price}</p>
+
+              <Button
+                size="sm"
+                className="ml-auto"
+                onClick={() => add({ id, title, description, image, price })}
+              >
+                + CARRINHO
+              </Button>
+            </CardFooter>
+          </Card>
+        </article>
+      )}
+    </>
+  );
+}
---
+++ src/modules/home/components/ProductCard/types.ts
@@ -0,0 +1,7 @@
+export type CardProps = {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+  price: number;
+};
