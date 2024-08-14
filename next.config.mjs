6ceef14d076685f8bbe4d1130f89eaf9361989f1
luiz2k <luizteles934@gmail.com>
2024-08-14 20:37:16 -0300

    Configurar o remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        port: "",
+        pathname: "/**.jpeg",
+      },
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+        port: "",
+        pathname: "/**.jpg",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
